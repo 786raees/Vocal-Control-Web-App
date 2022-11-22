c9fc1c14a47616a3bbe1b065aa5fd48bd098a350
Waqar Khan <96701299+786raees@users.noreply.github.com>
2022-11-22 09:47:21 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python src/manage.py migrate --no-input
-python src/manage.py collectstatic --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
-DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python src/manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
 gunicorn src.Eshop.wsgi:application --bind 0.0.0.0:8000
